70027eb2ab4c0e33a42e488b52e4ea860e5b3e48
kevinseya <kevinseya@gmail.com>
2025-02-07 02:17:22 -0500

    perf: :white_check_mark: Update management of Websocket Service

---
+++ src/main/java/com/microserviciologistic/createuser/websocket/WebSocketMessageHandler.java
@@ -0,0 +1,19 @@
+package com.microserviciologistic.createuser.websocket;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.microserviciologistic.createuser.model.WebSocketEvent;
+import org.springframework.stereotype.Service;
+
+@Service
+public class WebSocketMessageHandler {
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    public void processMessage(String message) {
+        try {
+            WebSocketEvent event = objectMapper.readValue(message, WebSocketEvent.class);
+            System.out.println("Event recived: " + event);
+        } catch (Exception e) {
+            System.err.println(" Error to process the message: " + e.getMessage());
+        }
+    }
+}
